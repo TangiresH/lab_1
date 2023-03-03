50c786d7b7ecc967665ff84ed82bb3db5a1c72b4
GhostDolphin <d.ghost2ac@gmail.com>
2023-03-03 15:02:40 +0200

    Added handler and created main function

--- lab1.go
+++ lab1.go
@@ -4,3 +4,12 @@ const (
 	path = "/time"
 	port = ":8795"
 )
+
+type handler struct{}
+
+func (h *handler) ServeHTTP(res http.ResponseWrite, req *http.Request) {
+	if req.Method != http.MethodGet || req.URL.Path != path {
+		http.Error(res, "Not found", http.StatusNotFound)
+		return
+	}
+}
